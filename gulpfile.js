2afa9dc2b10d353e35ead66b033e7627930a10e8
Davi Arcos Baptista <cl1621@fiap.com.br>
2019-06-28 13:44:45 -0300

    web-workflow | gulp cssnano installed and watch task running

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass'); 
 const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const browserSync = require('browser-sync').create();
+
 
 sass.compiler = require('node-sass');
 
@@ -9,10 +12,30 @@ function defaultTask(cb) {
 }
 
 gulp.task('sass', () => {
-    return gulp.src('assets/sass/**/*.scss')
+    return gulp.src('assets/sass/style.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(autoprefixer({}))
-      .pipe(gulp.dest('./css'));
+      .pipe(gulp.dest('./assets/css'));
 });
+
+gulp.task('css', () => {
+    return gulp.src('./assets/css/style.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('./out'));
+});
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', () => {
+    gulp.watch(`./assets/**/*.scss`, gulp.series('sass'));
+    gulp.watch(`./css/**/*.css`, gulp.series('css'));
+})
+
 
 exports.default = defaultTask
